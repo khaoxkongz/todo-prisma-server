ed8bfa5786c931a72e4aeef973e052673c47a7ef
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-19 00:51:56 +0700

    feat(user): Add infinite loop to periodically run expirer
    - Implement expirer function to run expire in an infinite loop
    - Catch and log errors during token expiration
    - Delay loop execution by 1 minute between iterations

--- src/expirer.ts
+++ src/expirer.ts
@@ -1,4 +1,5 @@
 import { RedisClientType } from "redis";
+import { keyBlacklist, keyJwtExpire } from "./repositories/blacklist";
 
 export async function expirer(redis: RedisClientType<any, any, any>) {
 	while (1) {
@@ -12,4 +13,32 @@ export async function expirer(redis: RedisClientType<any, any, any>) {
 		// Every minute
 		await new Promise((r) => setTimeout(r, 60 * 1000));
 	}
+}
+
+async function expire(redis: RedisClientType<any, any, any>) {
+	// Get all blacklisted tokens
+	const blacklisteds = await redis.sMembers(keyBlacklist);
+	const now = Date.now();
+
+	blacklisteds.forEach(async (token) => {
+		const expStr = await redis.hGet(keyJwtExpire, token);
+		if (!expStr) {
+			return;
+		}
+
+		const exp = Number(expStr) * 1000;
+		if (isNaN(exp)) {
+			console.error(`found non-number exp: ${expStr}`);
+			return;
+		}
+
+		console.log({ now, token, exp });
+
+		if (now >= exp) {
+			console.log(`expiring ${token} at ${now}`);
+			await Promise.all([redis.sRem(keyBlacklist, token), redis.hDel(keyJwtExpire, token)]);
+			// await redis.sRem(keyBlacklist, token);
+			// await redis.hDel(keyJwtExpire, token);
+		}
+	});
 }
