60959c56565f2fea0665cd366632068d83179c8d
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-19 14:05:54 +0700

    feat(handlers): implement todo handler
    - Implement handlerTodo class
    - Add createTodo method to handle todo creation
    - Add getTodos method to retrieve all user's todos
    - Add getTodo method to retrieve a specific todo by id

--- src/handler/todo.ts
+++ src/handler/todo.ts
@@ -22,3 +22,70 @@ export interface IHandlerTodo {
   deleteTodos: HandlerFunc<JwtAuthRequest<IEmpty, IEmpty>>;
 }
 
+export function newHandlerTodo(repoTodo: IRepositoryTodo): IHandlerTodo {
+  return new handlerTodo(repoTodo);
+}
+
+class handlerTodo implements IHandlerTodo {
+  private repo: IRepositoryTodo;
+
+  constructor(repo: IRepositoryTodo) {
+    this.repo = repo;
+  }
+
+  public async createTodo(req: JwtAuthRequest<IEmpty, IWithMsg>, res: Response): Promise<Response> {
+    const { msg } = req.body;
+    if (!msg) {
+      return res.status(400).json({ error: "missing msg in json body" }).end();
+    }
+
+    // const ownerid = req.payload.id;
+    const { id: ownerId } = req.payload;
+    if (!ownerId) {
+      return res.status(400).json({ error: "missing payload id" }).end();
+    }
+
+    return this.repo
+      .createTodo({ msg, ownerId })
+      .then((todo) => res.status(201).json(todo).end())
+      .catch((err) => {
+        const errMsg = `failed to create todo`;
+        console.error(`${errMsg}: ${err}`);
+        return res.status(500).json({ error: errMsg }).end();
+      });
+  }
+
+  public async getTodos(req: JwtAuthRequest<IEmpty, IEmpty>, res: Response): Promise<Response> {
+    return this.repo
+      .getUserTodos(req.payload.id)
+      .then((todos) => res.status(200).json({ data: todos }).end())
+      .catch((err) => {
+        const errMsg = `failed to get all todo`;
+        console.error(`${errMsg}: ${err}`);
+        return res.status(500).json({ error: errMsg }).end();
+      });
+  }
+
+  public async getTodo(req: JwtAuthRequest<IWithId, IEmpty>, res: Response): Promise<Response> {
+    const id = Number(req.params.id);
+    // isNaN checks if its arg is NaN
+    if (isNaN(id)) {
+      return res.status(400).json({ error: `id ${req.params.id} is not a number` });
+    }
+
+    return this.repo
+      .getUserTodoById({ id, ownerId: req.payload.id })
+      .then((todo) => {
+        if (!todo) {
+          return res.status(404).json({ error: `no such todo: ${id} ` });
+        }
+
+        return res.status(200).json(todo).end();
+      })
+      .catch((err) => {
+        const errMsg = `failed to get todo ${id}`;
+        console.error(`${errMsg}: ${err}`);
+        return res.status(500).json({ error: errMsg });
+      });
+  }
+}
